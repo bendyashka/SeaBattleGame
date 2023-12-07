bf94b2103e7aafbf574a56e1eac5f270dcf2f7f4
bendyashka <tu.dastan.tu@gmail.com>
2023-12-07 19:36:16 +0600

    Create Main.java
    структура кода, точнее проработка логики доски

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        char[][] theGround = new char[7][7];
+        gameboard(theGround);
+        putShips();
+    }
+
+    private static void putShips() {
+    }
+
+    private static void gameboard(char[][] theGround) {
+    }
+}

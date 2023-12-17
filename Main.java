adce99022d35fdc265ba68f3691d50957b68ab83
bendyashka <tu.dastan.tu@gmail.com>
2023-12-17 14:37:52 +0600

    Update Main.java
    пытаюсь сделать интерфейс, пока все идет жопой, ничего не работает, я пытаюсь понять почему

--- Main.java
+++ Main.java
@@ -7,29 +7,106 @@ public class Main {
         Random random = new Random();
 
         char[][] theGround = new char[7][7];
+        char[][] hiddenGround = new char[7][7];
         int vertical = 1;
         int horizontal = 2;
-        gameboard(theGround);
-        putShips(theGround);
-        putMediumShips(theGround);
-        putMediumShips(theGround);
-        putSingleShips(theGround);
-        actualGameBoard(theGround);
+        gameboard(theGround, hiddenGround);
+        putShips(theGround, hiddenGround);
+        putMediumShips(theGround, hiddenGround);
+        putMediumShips(theGround, hiddenGround);
+        putSingleShips(theGround, hiddenGround);
         actualGameBoard(theGround);
 
+
+        while (true) {
+            makeShot(theGround, hiddenGround, scanner);
+            actualGameBoard(hiddenGround);
+
+            if (isGameFinished(hiddenGround)) {
+                System.out.println("All ships Destroyed!");
+                break;
+            }
+        }
     }
 
-    private static void actualGameBoard(char[][] theGround) {
+    private static void makeShot(char[][] theGround, char[][] hiddenGround, Scanner scanner) {
+        System.out.println("\033[H\033[2J");  // Очистка консоли
+        actualGameBoard(hiddenGround);
+        System.out.print("Input coordinates (example:  1:1): ");
+        String input = scanner.nextLine();
+        String[] coordinates = input.split(":");
+
+        int row = Integer.parseInt(coordinates[0]) - 1;
+        int col = Integer.parseInt(coordinates[1]) - 1;
+
+        if (isValidShot(row, col) && hiddenGround[row][col] == '&') {
+            char target = theGround[row][col];
+            hiddenGround[row][col] = (target != '&') ? 'X' : '*';
+
+            if (target != '&') {
+                System.out.println("Hit!");
+                char hit = 'h';
+                hiddenGround[row][col] =hit;
+
+                if (isShipDestroyed(theGround, hiddenGround, target)) {
+                    System.out.println("Ship Destoyed!");
+                    char destroyed = 'd';
+                    hiddenGround[row][col] =destroyed;
+
+                }
+            } else {
+                System.out.println("Miss!");
+                char miss = 'm';
+                hiddenGround[row][col] = miss;
+
+            }
+        } else {
+            System.out.println("Некорректные координаты или вы уже стреляли в эту ячейку. Попробуйте еще раз.");
+        }
+    }
+
+
+    private static boolean isValidShot(int row, int col) {
+        return row >= 0 && row < 7 && col >= 0 && col < 7;
+    }
+
+    private static boolean isShipDestroyed(char[][] theGround, char[][] hiddenGround, char shipType) {
+        for (int i = 0; i < theGround.length; i++) {
+            for (int j = 0; j < theGround[i].length; j++) {
+                if (theGround[i][j] == shipType && hiddenGround[i][j] != 'X') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isGameFinished(char[][] theGround, char[][] hiddenGround) {
+        for (int i = 0; i < theGround.length; i++) {
+            for (int j = 0; j < theGround[i].length; j++) {
+                char target = theGround[i][j];
+                if ((target == 'S' || target == 'M' || target == 'L') && hiddenGround[i][j] == '&') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    private static void actualGameBoard(char[][] hiddenGround) {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 7; j++) {
-                System.out.print(theGround[i][j] + " ");
+                System.out.print(hiddenGround[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    private static void putSingleShips(char[][] theGround) {
+    private static void putSingleShips(char[][] theGround, char[][] hiddenGround) {
         Random random = new Random();
         int vertical = 1;
         int horizontal = 2;
@@ -64,13 +141,12 @@ public class Main {
 
                 if (isValidPlacement) {
                     theGround[randomNum][randomNum2] = ship;
-                //    System.out.println("1: " + randomNum + " " + randomNum2);
+                    hiddenGround[randomNum][randomNum2] = '&';
                     clearPlace = true;
                 }
             }
         }
     }
-
 
     public static int[] randomizecoord(int[] data) {
         Random random = new Random();
@@ -86,7 +162,7 @@ public class Main {
         return result;
     }
 
-    private static void putMediumShips(char[][] theGround) {
+    private static void putMediumShips(char[][] theGround, char[][] hiddenGround) {
         Random random = new Random();
         int vertical = 1;
         int horizontal = 2;
@@ -142,29 +218,16 @@ public class Main {
 
                 if (neigboorPlaceClear) {
                     theGround[randomNum][randomNum2] = ship1;
-
-                    if (directionOfBigShip == vertical) {
-                        theGround[randomNum + 1][randomNum2] = ship1;
-                    } else {
-                        theGround[randomNum][randomNum2 + 1] = ship1;
-                    }
-
-                    // System.out.println("2: " + randomNum + " " + randomNum2);
+                    theGround[randomNum + 1][randomNum2] = ship1;
+                    hiddenGround[randomNum][randomNum2] = '&';
+                    hiddenGround[randomNum + 1][randomNum2] = '&';
                     clearplace = true;
                 }
             }
         }
     }
 
-
-
-
-
-
-
-
-
-    private static void putShips(char[][] theGround) {
+    private static void putShips(char[][] theGround, char[][] hiddenGround) {
         Random random = new Random();
         int vertical = 1;
         int horizontal = 2;
@@ -174,27 +237,23 @@ public class Main {
         int randomNum = random.nextInt(maximumSizeOfBigShip - minimumSizeOfBigShip + 1) + minimumSizeOfBigShip;
         int randomNum2 = random.nextInt(maximumSizeOfBigShip - minimumSizeOfBigShip + 1) + minimumSizeOfBigShip;
         int directionOfBigShip = random.nextInt(horizontal - vertical + 1) + vertical;
-       // System.out.println(randomNum + " " + randomNum2 + " " + directionOfBigShip);
         char ship = 'S';
 
         if (randomNum >= 0 && randomNum < 7 && randomNum2 >= 0 && randomNum2 < 7) {
             theGround[randomNum][randomNum2] = ship;
-
-            if (directionOfBigShip == vertical) {
-                theGround[randomNum + 1][randomNum2] = ship;
-                theGround[randomNum - 1][randomNum2] = ship;
-            } else {
-                theGround[randomNum][randomNum2 + 1] = ship;
-                theGround[randomNum][randomNum2 - 1] = ship;
-            }
-
+            theGround[randomNum + 1][randomNum2] = ship;
+            theGround[randomNum - 1][randomNum2] = ship;
+            hiddenGround[randomNum][randomNum2] = '&';
+            hiddenGround[randomNum + 1][randomNum2] = '&';
+            hiddenGround[randomNum - 1][randomNum2] = '&';
         }
     }
 
-    public static void gameboard(char[][] theGround) {
+    public static void gameboard(char[][] theGround, char[][] hiddenGround) {
         for (int i = 0; i < theGround.length; i++) {
             for (int j = 0; j < theGround[i].length; j++) {
                 theGround[i][j] = '&';
+                hiddenGround[i][j] = '&';
             }
         }
 
